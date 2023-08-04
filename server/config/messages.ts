d26287d54bcf9c6816c33143030f3ceda270f370
Bas-sheikh <basilelshakhe@gmail.com>
2023-08-04 23:35:06 +0300

    created messages enum that includes the repeated messages. relates #8

---
+++ server/config/messages.ts
@@ -0,0 +1,11 @@
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable no-shadow */
+/* eslint-disable no-unused-vars */
+export enum Message {
+  SUCCESS = "Data received successfully",
+  LOGIN = "You are logged in successfully",
+  REGISTER = "You are signed up successfully",
+  NOT_FOUND = "Not Found",
+  VALIDATION_ERROR = "Check your input",
+  DELETE_FAILED = "Delete failed",
+}
