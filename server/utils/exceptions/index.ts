7ad8f7ef8d2bed8b559b920e8eeccd9e68a152f0
Bas-sheikh <basilelshakhe@gmail.com>
2023-08-05 22:36:41 +0300

    created error exceptions such as forbidden, bad request ..., and not found. relates #15

---
+++ server/utils/exceptions/index.ts
@@ -0,0 +1,11 @@
+import NotFoundException from "./NotFoundException";
+import BadRequestException from "./BadRequestException";
+import ForbiddenException from "./ForbiddenException";
+import UnauthorizedException from "./UnauthorizedException";
+
+export {
+  NotFoundException,
+  BadRequestException,
+  ForbiddenException,
+  UnauthorizedException,
+};
