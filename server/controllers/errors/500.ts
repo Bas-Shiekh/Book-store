3e6a57a7c643925e145112945ec0bd8fe13c719a
Bas-sheikh <basilelshakhe@gmail.com>
2023-08-05 22:02:48 +0300

    removed errors folders because errors handled in app.js file. relates #15

--- server/controllers/errors/500.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-unused-vars */
-import { ErrorRequestHandler } from 'express';
-
-const serverError: ErrorRequestHandler = (error, req, res, next) => {
-  res.status(error.status || 500).json({ message: error.message || 'Internal server error!' });
-};
-
-export default serverError;
