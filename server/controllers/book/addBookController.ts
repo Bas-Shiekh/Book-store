e4c1a6dee5d32e5c200c968373cc9c8138c3fe84
Bas-sheikh <basilelshakhe@gmail.com>
2023-08-05 22:25:29 +0300

    handled add new book controller. relates #15

---
+++ server/controllers/book/addBookController.ts
@@ -0,0 +1,42 @@
+/* eslint-disable dot-notation */
+import { Response } from "express";
+import { UserRequestInterface } from "../../interfaces/payload";
+import { addBookValidation } from "../../validation/book";
+import { addBookQuery } from "../../queries/books";
+import { Message } from "../../config/messages";
+import findBookByTitleQuery from "../../queries/books/findBookByTitleQuery";
+import { BadRequestException } from "../../utils/exceptions";
+
+const addBookController = async (req: UserRequestInterface, res: Response) => {
+  const { title, description, publicationYear, author, category, price } = req.body;
+  const { id } = req.user;
+
+  await addBookValidation({
+    title,
+    description,
+    publicationYear,
+    author,
+    category,
+    price,
+  });
+
+  const bookExist = await findBookByTitleQuery(title);
+  if (bookExist) throw new BadRequestException("The Book is already exist");
+
+  const book: any = {
+    title,
+    description,
+    price,
+    user_id: id,
+  };
+
+  if (publicationYear) book["publication_year"] = publicationYear;
+  if (author) book["author"] = author;
+  if (category) book["category"] = category;
+
+  const bookData = await addBookQuery(book);
+
+  res.json({ data: bookData, message: Message.CREATE_BOOK });
+};
+
+export default addBookController;
