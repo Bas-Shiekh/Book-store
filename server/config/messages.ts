6d07ee8f40f128a930fe4c89c1218b78683b89e4
Bas-sheikh <basilelshakhe@gmail.com>
2023-08-05 22:13:12 +0300

    added create book message. relates #15

--- server/config/messages.ts
+++ server/config/messages.ts
@@ -6,6 +6,6 @@ export enum Message {
   LOGIN = "You are logged in successfully",
   REGISTER = "You are signed up successfully",
   NOT_FOUND = "Not Found",
-  VALIDATION_ERROR = "Check your input",
   DELETE_FAILED = "Delete failed",
+  CREATE_BOOK = "The book has been created successfully",
 }
