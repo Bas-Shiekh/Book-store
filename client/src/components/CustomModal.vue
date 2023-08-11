7f6d790973eb7df1d022b3a5ce4626c89a04ec8f
Bas-sheikh <basilelshakhe@gmail.com>
2023-08-11 23:06:57 +0300

    handled customModal component that takes a children components. relates #33

---
+++ client/src/components/CustomModal.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="$props.dialog" persistent max-width="800px">
+      <slot></slot>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+export default {
+  props: {
+    toggleDialog: {
+      type: Function,
+    },
+    dialog: {
+      type: Boolean,
+    },
+  },
+  methods: {
+    handleSubmit() {
+      this.customFunction();
+    },
+  },
+};
+</script>
